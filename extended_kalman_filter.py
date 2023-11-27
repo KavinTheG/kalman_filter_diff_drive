c51912753c217abfec4e11911d06e0d02f84db85
Kavin Gnanapandithan <kavin.balag@gmail.com>
2023-11-26 23:12:47 -0500

    Update, there are errors to be fixed

--- extended_kalman_filter.py
+++ extended_kalman_filter.py
@@ -6,14 +6,14 @@ class ExtendedKalmanFilter:
 
         # x_k
         self.X_posteriori = X
-        self.x = self.X_posteriori[0]
-        self.y = self.X_posteriori[1]
-        self.theta = self.X_posteriori[2]
+        self.x = self.X_posteriori[0][0]
+        self.y = self.X_posteriori[1][0]
+        self.theta = self.X_posteriori[2][0]
         self.v = v
         self.X_previous = self.X_posteriori
 
         # x_k^-
-        self.a_priori_state = X
+        self.X_priori = X
 
         self.Q = Q
         self.R = R
@@ -27,23 +27,23 @@ class ExtendedKalmanFilter:
 
         # Set up matrix A
         self.A = np.eye(3)
-        self.A[0][2] = v * self.deltaT * np.sin(self.theta)
+        self.A[0][2] = -v * self.deltaT * np.sin(self.theta)
+        self.A[1][2] = v * self.deltaT * np.cos(self.theta)
 
         # Set up matrix B
         self.B = np.zeros((3,2))
         # delta theta is equal self.theta - self.X_prev[2]
-        self.B[0][0] = self.deltaT * np.cos(self.theta + (self.theta - self.X_previous[2]) / 2 )
-        self.B[1][0] = self.deltaT * np.sin(self.theta + (self.theta - self.X_previous[2]) / 2 )
+        self.B[0][0] = self.deltaT * np.cos(self.theta + (self.theta - self.X_previous[2][0]) / 2 )
+        self.B[1][0] = self.deltaT * np.sin(self.theta + (self.theta - self.X_previous[2][0]) / 2 )
         self.B[2][1] = 1
 
         self.C = np.eye(3)
 
 
 
-
-
-
     def predict(self, U):
+
+        self.theta = self.X_previous[2][0]
         # Set up Q1
         Q1 = [
               [self.Q[0][0] * self.deltaT + self.Q[2][2]*(self.deltaT**3 /3)* self.v**2 * np.sin(self.theta)**2],
@@ -65,29 +65,50 @@ class ExtendedKalmanFilter:
               [self.Q[2][2] * self.deltaT ]
         ]
 
-        # Set up Qk
-        self.Qk = [Q1, Q2, Q3]
+        self.Qk = np.concatenate((Q1, Q2, Q3), axis=1)
 
-        # x_{k+1}^-
-        self.X_priori = self.X_previous + self.B * U
+        # Set up matrix A
+        self.A = np.eye(3)
+        self.A[0][2] = -self.v * self.deltaT * np.sin(self.theta)
+        self.A[1][2] = self.v * self.deltaT * np.cos(self.theta)
 
-        # P_{k+1}^-
-        self.P_priori = self.A * self.P_previous * self.A.T + self.Qk
+        # Set up matrix B
+        self.B = np.zeros((3,2))
+        # delta theta is equal self.theta - self.X_prev[2]
+        self.B[0][0] = self.deltaT * np.cos(self.theta + (self.theta - self.X_previous[2][0]) / 2 )
+        self.B[1][0] = self.deltaT * np.sin(self.theta + (self.theta - self.X_previous[2][0]) / 2 )
+        self.B[2][1] = 1
 
+        self.X_priori = self.X_previous + np.dot(self.B, U)
+        
+        AxP = np.dot(self.A, self.P_previous)
+        AxPxA = np.dot(AxP, self.A.T)
+        self.P_priori = AxPxA + self.Qk
 
     
     def measure(self, Y):
-        
-        S = self.C * self.P_priori * self.C.T + self.R 
+        # CPC^T whre C is an identity matrix would simplify to P
+        S = self.P_priori + self.R 
+     
+        '''
+        K_{k+1} 
+        This boils down to P / P + R
+        If R ~ 0, Kg is ~ 1 and vice-versa
+        ''' 
+        self.kalman_gain = np.dot(self.P_priori, np.linalg.inv(S)) 
 
-        # K_{k+1}
-        self.kalman_gain = self.P_priori * self.C.T / S
-
+        residual = Y - self.X_priori
+        state_or_sensor = np.dot(self.kalman_gain, residual)
+    
         # x_{k+1}
-        self.X_posteriori = self.X_priori + self.kalman_gain * (Y - self.C * self.X_priori)
+        self.X_posteriori = self.X_priori + state_or_sensor
         self.X_previous = self.X_posteriori
 
         # P_{k+1}
-        self.P_posteriori = (np.eye(3) - self.kalman_gain * self.C) * self.P_priori
+        self.P_posteriori = np.dot((np.eye(3) - self.kalman_gain), self.P_priori)
         self.P_previous = self.P_posteriori
 
+
+
+    def get_posteriori(self):
+        return self.X_posteriori
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from extended_kalman_filter import ExtendedKalmanFilter
 import numpy as np
 from pprint import pprint
-import matplotlib
+import matplotlib.pyplot as plt
 import time
 import random
 
@@ -12,7 +12,7 @@ import random
 R = np.eye(3) * 0.1
 
 # Assume kinematic model / inputs has std of 0.1m
-Q = np.eye(3) * 0.1
+Q = np.eye(3) * 0.01
 
 # Initialize error covariance matrix
 P = np.eye(3)
@@ -38,12 +38,14 @@ x_0 = 0
 y_0 = 0
 theta_0 = 0
 
-# Track true movements of robot
+# Track movements of robot using velocity linear and angular (w/o noise)
+# I will add noise to this to represent odometry data
 x_true = 0
 y_true = 0
 theta_true = 0
 
 # Tracking of robot from kinematics
+# 
 x_kin = 0
 y_kin = 0
 theta_kin = 0
@@ -54,63 +56,113 @@ y_sensor = 0
 theta_sensor = 0
 
 # Initial Inputs
-x_dot = v * np.cos(0)
-y_dot = v * np.sin(0)
+x_dot = v * np.cos(omega)
+y_dot = v * np.sin(omega)
 theta_dot = omega
 
 x = [x_0, y_0, theta_0]
+x_np = [[x_0], [y_0], [theta_0]]
+kf = ExtendedKalmanFilter(x_np, Q, R, P, v, dt)
 
-kf = ExtendedKalmanFilter(x, Q, R, P, v)
+
+X_posteriori = [x_0, y_0, theta_0]
+
+
+
+# Plotting variables
+kinematic_plot_x = []
+sensor_plot_x = []
+kinematic_plot_y = []
+sensor_plot_y = []
+
+kalman_x = []
+kalman_y = []
+
 
 for _ in range (int(total_time / dt)):
 
     # Control input noise
-    w = np.random.multivariate_normal(mean=np.zeros(3), cov=Q, size=1).T
+    w = np.random.multivariate_normal(mean=np.zeros(3), cov=Q, size=1)
+    print()
     theta_true += dt * theta_dot
     # calculate x and y velocity from theta angle
     x_dot = v * np.cos(theta_true)
     y_dot = v * np.sin(theta_true)
+    theta_dot = omega
 
     x_true += dt * x_dot
     y_true += dt * y_dot
-
-    theta_dot = omega
-
+    X_dot_true = [x_dot, y_dot, theta_dot]
 
     # Control input
-    U = [v, omega]
-
-    factor = random.choice([-1, 1])
-    offset = random.random()
+    U = [[v], [omega]]
 
     print("Velocity: ", v, " Angular Velocity: ", theta_dot, "\n")
 
-    X_dot_true = [[x_dot], [y_dot], [theta_dot]]
-    X_state_true = [[x_true], [y_true], [theta_dot]]
+
+    X_state_true = [x_true, y_true, theta_true]
     print("---- True X ", _ ," ----")
     pprint(X_state_true)
 
 
     print("\n----Kinematic Model ----")
-    
-    X_dot_kin = X_dot_true + w
+
+    X_dot_kin = X_dot_true + w[0]
     x_kin += X_dot_kin[0] * dt
     y_kin += X_dot_kin[1] * dt
     theta_kin += X_dot_kin[2] * dt
-    X_state_kin = [[x_kin], [y_kin], [theta_kin]]
-    pprint(X_state_kin)
+
+    X_state_kin = [x_kin, y_kin, theta_kin]
+    print("Kinematic State : ", X_state_kin)
+    # print("Kinematic (velocity): ", X_dot_kin)
 
     
     print("---Sensor Model----")
     # Y value
-    n = np.random.multivariate_normal(mean=np.zeros(3), cov=R, size=1).T
-    X_dot_sensor = X_dot_true + n
+    n = np.random.multivariate_normal(mean=np.zeros(3), cov=R, size=1)
+    X_dot_sensor = X_dot_true + n[0]
     x_sensor += X_dot_sensor[0] * dt
     y_sensor += X_dot_sensor[1] * dt
     theta_sensor += X_dot_sensor[2] * dt
-    X_state_sensor = [[x_sensor], [y_sensor], [theta_sensor]]
+    X_state_sensor = [x_sensor, y_sensor, theta_sensor]
+
+    # Array to give  to kf to handle np array
+    X_state_sensor_np = [[x_sensor], [y_sensor], [theta_sensor]]
     pprint(X_state_sensor)
     
 
-    print("\n")
-    time.sleep(0.5)
+
+    print("\n---Kalman Filter----")
+    kf.predict(U)
+    kf.measure(X_state_sensor_np)
+    kf_state = kf.get_posteriori()
+    print(kf_state)
+    
+
+    ### Plot ###
+    kinematic_plot_x.append(x_kin)
+    sensor_plot_x.append(x_sensor)
+
+    kinematic_plot_y.append(y_kin)
+    sensor_plot_y.append(y_sensor)
+
+    kalman_x.append(kf_state[0][0])
+    kalman_y.append(kf_state[1][0])
+
+    #time.sleep(0.5)
+print(kf.get_posteriori())
+
+plt.plot(kinematic_plot_x, kinematic_plot_y, color='red', label='Kinematic Model')
+plt.plot(sensor_plot_x, sensor_plot_y, color='green', label='Odom Data')
+plt.plot(kalman_x, kalman_y, color='blue', label='Kalman' )
+
+# Add labels and title
+plt.xlabel('X-axis Label')
+plt.ylabel('Y-axis Label')
+
+# Add x and y axes at origin
+plt.axhline(0, color='black', linewidth=0.5)  # Horizontal line at y=0
+plt.axvline(0, color='black', linewidth=0.5)  # Vertical line at x=0
+
+plt.legend()
+plt.show()
